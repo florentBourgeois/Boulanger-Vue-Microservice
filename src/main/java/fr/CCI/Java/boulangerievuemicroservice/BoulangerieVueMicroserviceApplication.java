736cf04906aba69047850f81b7c123ec77009d3b
Florent.Bourgeois <florent.bourgeois@uha.fr>
2019-04-24 18:38:30 +0200

    initial commit : mise à disposition du squellette de l'app et du pom avec les dependences feign

---
+++ src/main/java/fr/CCI/Java/boulangerievuemicroservice/BoulangerieVueMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package fr.CCI.Java.boulangerievuemicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+public class BoulangerieVueMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoulangerieVueMicroserviceApplication.class, args);
+	}
+
+}
