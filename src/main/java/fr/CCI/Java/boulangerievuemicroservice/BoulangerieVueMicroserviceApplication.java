8e159bb16a5f62047c0346c52eea3e438659f700
Florent.Bourgeois <florent.bourgeois@uha.fr>
2019-04-24 18:39:28 +0200

    ajout de l'annotation pour trouver les client feign dans l'application

--- src/main/java/fr/CCI/Java/boulangerievuemicroservice/BoulangerieVueMicroserviceApplication.java
+++ src/main/java/fr/CCI/Java/boulangerievuemicroservice/BoulangerieVueMicroserviceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("boulangerie")
 public class BoulangerieVueMicroserviceApplication {
 
 	public static void main(String[] args) {
